ccc0a2d366a12e0620072a1d545606dbe6c329e0
Dong Ky <kyhuudong@gmail.com>
2021-04-27 21:11:57 +0700

    Update(Load data): Finished load data to DW

--- modules/edsda-etl/database/perform_edsda.sh
+++ modules/edsda-etl/database/perform_edsda.sh
@@ -1,11 +1,12 @@
 #!/bin/bash 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
+#Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 directory=$SCRIPTPATH
 tables="/create_tables.py"
 fileperform="/perform_etl.py"
+loaddatabase="/load_data.py"
 
 
 cd ..
@@ -28,6 +29,7 @@ cd $directory
 
 python3 $directory$tables
 python3 $directory$fileperform
+python3 $directory$loaddatabase
 
 
 
